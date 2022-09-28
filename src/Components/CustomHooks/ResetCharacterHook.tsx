62df3c4aa50d56ce3f143154c5932d40319e1120
Brendan Hurd <brendanhurd@gmail.com>
2022-09-28 16:21:54 +1000

    Refactor the ResetCharacterHook to remove error.

--- src/Components/CustomHooks/ResetCharacterHook.tsx
+++ src/Components/CustomHooks/ResetCharacterHook.tsx
@@ -3,7 +3,10 @@ import { useCharacterStore } from 'Global State/store';
 export const useResetCharacter = () => {
   const resetSelect = useCharacterStore((state) => state.resetSelectedOptions);
   const resetCharacter = useCharacterStore((state) => state.resetCharacter);
+  const selectedOptions = useCharacterStore((state) => state.selectedOptions);
 
-  resetSelect();
-  resetCharacter();
+  if (selectedOptions.length > 0) {
+    resetSelect();
+    resetCharacter();
+  }
 };
